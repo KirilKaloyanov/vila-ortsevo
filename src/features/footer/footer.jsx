b8c7456649475e457f0508c8dcb313f87696ee42
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2024-02-20 08:13:25 +0200

    typo

--- src/features/footer/footer.jsx
+++ src/features/footer/footer.jsx
@@ -40,7 +40,7 @@ export default function Footer() {
       </div>
       <div className="credit">
         <center>
-          Web design by <u>cosmoswebstudios</u>
+          Web design by <u>cosmoswebstudio</u>
         </center>
       </div>
     </div>
